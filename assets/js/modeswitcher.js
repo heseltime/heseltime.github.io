6f363c73191c824e8344c6e940e9e24f8070be48
Jack Heseltine <66922223+heseltime@users.noreply.github.com>
2022-12-31 14:06:49 +0100

    bug fix on load

--- assets/js/modeswitcher.js
+++ assets/js/modeswitcher.js
@@ -2,6 +2,7 @@
 Copied from https://github.com/derekkedziora/jekyll-demo/blob/master/scripts/mode-switcher.js
 https://github.com/derekkedziora/jekyll-demo
 Creative Commons Attribution 4.0 International License
+edutied by Jack Heseltine, 12/31/2022
 */
 
 let systemInitiatedDark = window.matchMedia("(prefers-color-scheme: dark)"); 
@@ -10,31 +11,22 @@ let theme = sessionStorage.getItem('theme');
 const iconSun = "/assets/img/sun.svg";
 const iconMoon = "/assets/img/moon.svg";
 
-
 function changeIconImgSrc(src) {
 	document.getElementById("theme-toggle-img").src = src;
 	document.getElementById("theme-toggle-img--mobile").src = src;
 }
 
-if (systemInitiatedDark.matches) {
-	changeIconImgSrc(iconMoon);
-} else {
-	changeIconImgSrc(iconSun);
-}
-
 function prefersColorTest(systemInitiatedDark) {
-  if (systemInitiatedDark.matches) {
-  	document.documentElement.setAttribute('data-theme', 'dark');		
-   	changeIconImgSrc(iconMoon);
-   	sessionStorage.setItem('theme', '');
-  } else {
-  	document.documentElement.setAttribute('data-theme', 'light');
-    changeIconImgSrc(iconSun);
-    sessionStorage.setItem('theme', '');
-  }
+if (systemInitiatedDark.matches) {
+	document.documentElement.setAttribute('data-theme', 'dark');		
+	changeIconImgSrc(iconMoon);
+	sessionStorage.setItem('theme', '');
+} else {
+	document.documentElement.setAttribute('data-theme', 'light');
+	changeIconImgSrc(iconSun);
+	sessionStorage.setItem('theme', '');
 }
-systemInitiatedDark.addListener(prefersColorTest);
-
+}
 
 function modeSwitcher() {
 	let theme = sessionStorage.getItem('theme');
@@ -57,12 +49,22 @@ function modeSwitcher() {
 	}
 }
 
-if (theme === "dark") {
-	document.documentElement.setAttribute('data-theme', 'dark');
-	sessionStorage.setItem('theme', 'dark');
-	changeIconImgSrc(iconMoon);
-} else if (theme === "light") {
-	document.documentElement.setAttribute('data-theme', 'light');
-	sessionStorage.setItem('theme', 'light');
-	changeIconImgSrc(iconSun);
-}
+document.addEventListener("DOMContentLoaded", function(event) {
+	if (systemInitiatedDark.matches) {
+		changeIconImgSrc(iconMoon);
+	} else {
+		changeIconImgSrc(iconSun);
+	}
+
+	systemInitiatedDark.addListener(prefersColorTest);
+
+	if (theme === "dark") {
+		document.documentElement.setAttribute('data-theme', 'dark');
+		sessionStorage.setItem('theme', 'dark');
+		changeIconImgSrc(iconMoon);
+	} else if (theme === "light") {
+		document.documentElement.setAttribute('data-theme', 'light');
+		sessionStorage.setItem('theme', 'light');
+		changeIconImgSrc(iconSun);
+	}
+});
